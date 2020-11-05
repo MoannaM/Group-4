0c91601b8ab5144accfd33b9d3bce48b973f95e1
MaitéMagistri <maite.magistri@unine.ch>
2020-11-05 16:41:10 +0100

    Salut :)

--- TP4Ex45.py
+++ TP4Ex45.py
@@ -1,22 +1,100 @@
-import statistics as s
-student_and_grade = {"Martina": 5.35, "Bastien": 5.07, "Claire": 3.53, "Anna":
-                    4.09, "Maria": 4.55, "Kim": 3.98,"Robin": 3.87, "Adriana": 5.20, "Kristina":
-                    5.33, "Michael": 4.52, "Sophie": 4.54, "Sara": 4.94,"Olof": 4.61, "Tina": 5.74,
-                    "Hanna": 4.42, "Mirsa": 5.55, "Sanna": 4.99, "Sally": 4.34,"Urban": 4.11,
-                    "Kelly": 5.14, "Helmer": 4.53, "Joanna": 4.69, "Josephine": 4.00, "Vilma": 5.19,
-                    "Martin": 5.35, "Bastiena": 5.07, "Klaire": 3.53, "Anne": 4.09, "Marie": 4.55,
-                    "Kimi": 4.98,"Robina": 3.87, "Adrian": 5.20, "Kristian": 3.3, "Michaelle": 3.52,
-                    "Sophia": 4.54, "Sarah": 4.94,"Olaf": 4.61, "Tino": 5.74, "Hanne": 4.42, "Mirso":
-                    3.55, "Sannah": 4.99, "Sallie": 4.34,"Urbi": 4.11, "Kellian": 5.14, "Helmut": 4.53,
-                    "Joan": 4.69, "Joseph": 4.00, "Vilmer": 5.19 }
+#!/usr/bin/env python3
+# by Seth Kenlon
 
-grade_average = s.mean(student_and_grade.values())
-students_above_avg = []
-for name, grade in student_and_grade.items():
-    if grade > grade_average:
-        students_above_avg.append(grade)
+# GPLv3
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from typing import Tuple
 
-print(grade_average)
-print(len(students_above_avg))
-print(student_and_grade)
+import pygame
+import sys
+import os
 
+'''
+Variables
+'''
+
+worldx = 960
+worldy = 720
+fps = 40  # frame rate
+ani = 4  # animation cycles
+world = pygame.display.set_mode([worldx, worldy])
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
+
+'''
+Objects
+'''
+
+
+class Player(pygame.sprite.Sprite):
+    """
+    Spawn a player
+    """
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
+            img.convert_alpha()  # optimise alpha
+            img.set_colorkey(ALPHA)  # set alpha
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+
+'''
+Setup
+'''
+
+backdrop = pygame.image.load(os.path.join('images', 'stage.png'))
+clock = pygame.time.Clock()
+pygame.init()
+backdropbox = world.get_rect()
+main = True
+
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 0  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+
+
+'''
+Main Loop
+'''
+
+while main:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+    world.blit(backdrop, backdropbox)
+    player_list.draw(world)
+    pygame.display.flip()
+    clock.tick(fps)
